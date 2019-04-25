2c36734759520ea8e568165260bc3b4a3da507c7
Richard Haines <richard.haines@88.131.110>
2019-04-25 21:27:40 +0200

    Changed button css

--- packages/gatsby-theme-hungry-bear-portfolio/src/components/Button.tsx
+++ packages/gatsby-theme-hungry-bear-portfolio/src/components/Button.tsx
@@ -41,86 +41,6 @@ export const buttonDark = css`
   }
 `;
 
-export const button = css`
-  display: inline-block;
-  text-decoration: none;
-  position: relative;
-  border: none;
-  outline: none;
-
-  & .bottom {
-    position: absolute;
-    left: 4px;
-    top: 7px;
-    width: 100%;
-    height: 100%;
-    background-color: ${colors.secondary};
-    display: block;
-    -webkit-transition: all 0.20s ease-out;
-    -moz-transition: all 0.20s ease-out;
-    -o-transition: all 0.20s ease-out;
-    transition: all 0.20s ease-out;
-  }
-
-  & .top {
-    position: relative;
-    left: 10px;
-    top: 0;
-    padding: 24px 34px 22px 34px;
-    border: 2px solid ${colors.primary};
-  }
-
-  & .top .label {
-    font-family: Montserrat;
-    font-weight: 600;
-    color: ${colors.primary};
-    font-size: 12px;
-    line-height: 110%;
-    letter-spacing: 2px;
-    text-align: center;
-    // text-transform: uppercase;
-    -webkit-transition: all 0.15s ease-out;
-    -moz-transition: all 0.15s ease-out;
-    -o-transition: all 0.15s ease-out;
-    transition: all 0.15s ease-out;
-  }
-
-  &:active {
-    ${buttonDark}
-  }
-
-  &:hover .bottom {
-    left: 10px;
-    top: 0;
-    background-color: ${colors.light};
-  }
-
-  &:hover .top .label {
-    color: ${colors.primary};
-    cursor: pointer;
-  }
-
-  &:hover
-    .top
-    .${buttonBorderLeft},
-    &:hover
-    .top
-    .${buttonBorderRight} {
-    height: calc(100% + 2px);
-    cursor: pointer;
-  }
-
-  &:hover
-    .top
-    .${buttonBorderTop},
-    &:hover
-    .top
-    .${buttonBorderBottom} {
-    width: calc(100% + 2px);
-    cursor: pointer;
-  }
-`;
-
 export const buttonBorder = css`
   position: absolute;
   background-color: ${colors.primary};
@@ -146,7 +66,87 @@ export const Button = ({text, onClick}: ButtonProps) => (
       },
     }}
   >
-    <a onClick={onClick} css={button}>
+    <a 
+      onClick={onClick} 
+      css={css`
+        display: inline-block;
+        text-decoration: none;
+        position: relative;
+        border: none;
+        outline: none;
+
+        & .bottom {
+          position: absolute;
+          left: 4px;
+          top: 7px;
+          width: 100%;
+          height: 100%;
+          background-color: ${colors.secondary};
+          display: block;
+          -webkit-transition: all 0.20s ease-out;
+          -moz-transition: all 0.20s ease-out;
+          -o-transition: all 0.20s ease-out;
+          transition: all 0.20s ease-out;
+        }
+
+        & .top {
+          position: relative;
+          left: 10px;
+          top: 0;
+          padding: 24px 34px 22px 34px;
+          border: 2px solid ${colors.primary};
+        }
+
+        & .top .label {
+          font-family: Montserrat;
+          font-weight: 600;
+          color: ${colors.primary};
+          font-size: 12px;
+          line-height: 110%;
+          letter-spacing: 2px;
+          text-align: center;
+          // text-transform: uppercase;
+          -webkit-transition: all 0.15s ease-out;
+          -moz-transition: all 0.15s ease-out;
+          -o-transition: all 0.15s ease-out;
+          transition: all 0.15s ease-out;
+        }
+
+        &:active {
+          ${buttonDark}
+        }
+
+        &:hover .bottom {
+          left: 10px;
+          top: 0;
+          background-color: ${colors.light};
+        }
+
+        &:hover .top .label {
+          color: ${colors.primary};
+          cursor: pointer;
+        }
+
+        &:hover
+          .top
+          .${buttonBorderLeft},
+          &:hover
+          .top
+          .${buttonBorderRight} {
+          height: calc(100% + 2px);
+          cursor: pointer;
+        }
+
+        &:hover
+          .top
+          .${buttonBorderTop},
+          &:hover
+          .top
+          .${buttonBorderBottom} {
+          width: calc(100% + 2px);
+          cursor: pointer;
+        }`}
+    >
         <div className="bottom"/>
         <div className="top">
             <div className="label">{text}</div>
